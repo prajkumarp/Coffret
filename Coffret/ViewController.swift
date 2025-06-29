01cf3486b0d826ce998864febcc5776c48b9b7bd
Rajkumar <prajkumarp@gmail.com>
2025-06-29 10:44:21 +0530

    Initial Commit

---
+++ Coffret/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coffret
+//
+//  Created by Rajkumar on 29/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CoffretTests/CoffretTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoffretTests.swift
+//  CoffretTests
+//
+//  Created by Rajkumar on 29/06/25.
+//
+
+import Testing
+@testable import Coffret
+
+struct CoffretTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
